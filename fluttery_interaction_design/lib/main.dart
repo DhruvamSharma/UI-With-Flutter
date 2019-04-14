77b9fad81d5bf109002729452b1627a300ee917f
Dhruvam <dhruvamssharma@gmail.com>
2019-04-14 22:55:17 +0530

    fonts added, overflow issue solved.

--- fluttery_interaction_design/lib/main.dart
+++ fluttery_interaction_design/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dogs_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,6 +20,10 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        primaryIconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(color: Colors.black,),
+        canvasColor: Colors.white,
+        fontFamily: 'NotoSans'
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -40,72 +45,98 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin<MyHomePage>{
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  TabController tabController;
+  MaterialColor color = Colors.teal;
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(vsync: this, length: 4)
+    ..addListener(() {
+      setState(() {
+        switch (tabController.index) {
+          case 0:
+            color = Colors.teal;
+            break;
+          case 1:
+            color = Colors.pink;
+            break;
+          case 2:
+            color = Colors.blue;
+            break;
+          case 3:
+            color = Colors.amber;
+            break;
+        }
+      });
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    tabController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+          title: Text("My Pet Stories",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 24
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircleAvatar(
+                radius: 14,
+                backgroundImage: AssetImage('assets/dog3.jpg'),
+              ),
+            )
           ],
+          bottom: TabBar(tabs: <Widget>[
+            Tab(icon: Icon(Icons.group, color: Colors.grey,), text: 'List',),
+            Tab(icon: Icon(Icons.repeat, color: Colors.grey,), text: 'Donate',),
+            Tab(icon: Icon(Icons.repeat, color: Colors.grey,), text: 'Meet',),
+            Tab(icon: Icon(Icons.repeat, color: Colors.grey,), text: 'Adopt',),
+          ],
+            indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(color: color, width: 4.0),
+                insets: EdgeInsets.fromLTRB(80, 20, 80, 0)
+            ),
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(color: Colors.black, fontSize: 16),
+            labelColor: Colors.black,
+            unselectedLabelStyle: TextStyle(fontSize: 0),
+            controller: tabController,
+          ),
         ),
+        body: TabBarView(children: <Widget>[
+          DogsList(),
+          DogsList(),
+          Center(child: Text('my data 2')),
+          Center(child: Text('my data 2')),
+        ],
+        controller: tabController,),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
